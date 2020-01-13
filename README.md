1d97cc21643953c5a76a500f610dd5a069b421a6
Mustafa Abdul-Kader <mustafa@muszr.me>
2020-01-13 12:51:25 -0600

    Cleanup the codebase

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
-##Algorithm
+## Algorithm
+
 Encode a 3 x 3 tic-tac-toe board into a single 18-bit string [EMPTY = 00, X = 10, O = 01]
+
 Create a mask for all non-empty tiles in a separate 18-bit string.
-Use predefined winning bitstrings (horizontal, vertical, diagnol)
-`/* 01 00 00 00 01 00 00 00 01 */ 0x10101,`
-`/* 00 00 01 00 01 00 01 00 00 */ 0x01110,`
-`/* 01 01 01 00 00 00 00 00 00 */ 0x15000,` shift thrice to drop a row
-`/* 01 00 00 01 00 00 01 00 00 */ 0x10410,` shift once to move a column
+Use predefined winning game-layout bitstrings (horizontal, vertical, diagonal)
+
+    /* 01 00 00 00 01 00 00 00 01 */ 0x10101,
+    /* 00 00 01 00 01 00 01 00 00 */ 0x01110,
+    /* 01 01 01 00 00 00 00 00 00 */ 0x15000, // shift thrice to drop a row
+    /* 01 00 00 01 00 00 01 00 00 */ 0x10410, // shift once to move a column
 
--- check-win.c
+++ check-win.c
@@ -1,80 +1,114 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define PLAYER_O 1
+#define PLAYER_X 2
+#define MASK 3
 
-void parse_file(char *fileName, int *gameBoard, int *gameMask)
+static const char *PLAYER_O_WINS = "O wins";
+static const char *PLAYER_X_WINS = "X wins";
+static const int winning_layouts[] = {
+	/* 01 00 00 00 01 00 00 00 01 */ 0x10101,
+	/* 00 00 01 00 01 00 01 00 00 */ 0x01110,
+	/* 01 01 01 00 00 00 00 00 00 */ 0x15000,
+	/* 01 00 00 01 00 00 01 00 00 */ 0x10410,
+};
+
+void parse_file(FILE * gameFile, int *gameBoard, int *gameMask)
 {
-    FILE *gameFile = fopen("game", "r");
-    // need 18 bits
-    int board = 0;
-    int mask = 0;
-    char line[3] = {0};
-    while (fscanf(gameFile, "%c | %c | %c\n", &line[0], &line[1], &line[2]) != EOF)
-    {
-        for (int i = 0; i < sizeof(line) / sizeof(char); i++)
-        {
-            board <<= 0x2;
-            mask <<= 0x2;
-            switch (line[i])
-            {
-            case '-':
-                board |= 0x0;
-                mask |= 0x0;
-                break;
-            case 'O':
-                board |= 0x1;
-                mask |= 0x3;
-                break;
-            case 'X':
-                board |= 0x2;
-                mask |= 0x3;
-                break;
-            default:
-                perror("Bad data.");
-                exit(EXIT_FAILURE);
-            }
-        }
-        *gameBoard = board;
-        *gameMask = mask;
-    }
-    if (ferror(gameFile) != 0)
-    {
-        perror("Failed to read game file.");
-        exit(EXIT_FAILURE);
-    }
-    fclose(gameFile);
+	// need 18 bits
+	int board = 0;
+	int mask = 0;
+	char line[3] = { 0 };
+	while (fscanf(gameFile, "%c | %c | %c\n", &line[0], &line[1], &line[2])
+	       != EOF) {
+		for (int i = 0; i < sizeof(line) / sizeof(char); i++) {
+			board <<= 2;
+			mask <<= 2;
+			switch (line[i]) {
+			case '-':
+			case ' ':
+				board |= 0;
+				mask |= 0;
+				break;
+			case 'O':
+				board |= PLAYER_O;
+				mask |= 3;
+				break;
+			case 'X':
+				board |= 2;
+				mask |= 3;
+				break;
+			default:
+				perror("Bad data.");
+				exit(EXIT_FAILURE);
+			}
+		}
+		*gameBoard = board;
+		*gameMask = mask;
+	}
+	if (ferror(gameFile) != 0) {
+		perror("Failed to read game file.");
+		exit(EXIT_FAILURE);
+	}
+	fclose(gameFile);
 }
-int checkWin(int winningGame, int bitField, int bitMask)
+
+unsigned char validate_winner(int winning_layout, int bitField, int bitMask)
 {
-    if ((bitField & winningGame) == winningGame)
-        return 10;
-    else if (((bitField ^ bitMask) & winningGame) == winningGame)
-        puts("X wins");
+	if ((bitField & winning_layout) == winning_layout) {
+		return PLAYER_X;
+	} else if (((bitField ^ bitMask) & winning_layout) == winning_layout) {
+		return PLAYER_O;
+	}
 }
+
+void print_winner(unsigned char winner)
+{
+	switch (winner) {
+	case PLAYER_O:
+		puts(PLAYER_O_WINS);
+		return;
+	case PLAYER_X:
+		puts(PLAYER_X_WINS);
+		return;
+	default:
+		return;
+	}
+}
+
 int main(void)
 {
-    int bitField, bitMask;
-    // parse tic-tac-toe board - 
-    parse_file("game", &bitField, &bitMask);
-    const int winningGames[] = {
-        /* 01 00 00 00 01 00 00 00 01 */ 0x10101,
-        /* 00 00 01 00 01 00 01 00 00 */ 0x01110,
-        /* 01 01 01 00 00 00 00 00 00 */ 0x15000,
-        /* 01 00 00 01 00 00 01 00 00 */ 0x10410,
-    };
-    checkWin(winningGames[0], bitField, bitMask);
-    checkWin(winningGames[1], bitField, bitMask);
-    int winningGame = winningGames[2];
-    for (int j = 0; j < 3; j++)
-    {
-        checkWin(winningGame, bitField, bitMask);
-        winningGame >>= 3;
-    }
-    winningGame = winningGames[3];
-    for (int j = 0; j < 3; j++)
-    {
-        checkWin(winningGame, bitField, bitMask);
-        winningGame >>= 1;
-    }
+	int bitField, bitMask;
+	FILE *gameFile = fopen("game", "r");
+	parse_file(gameFile, &bitField, &bitMask);
 
-    return 0;
+	unsigned char winner =
+	    validate_winner(winning_layouts[0], bitField, bitMask) ||
+	    validate_winner(winning_layouts[1], bitField, bitMask);
+	if (winner) {
+		print_winner(winner);
+		return 0;
+	}
+
+	int winning_layout = winning_layouts[2];
+	for (int j = 0; j < 3; j++) {
+		winner |= validate_winner(winning_layout, bitField, bitMask);
+		if (winner) {
+			print_winner(winner);
+			return 0;
+		}
+		winning_layout >>= 3;
+	}
+
+	winning_layout = winning_layouts[3];
+	for (int j = 0; j < 3; j++) {
+		winner |= validate_winner(winning_layout, bitField, bitMask);
+		if (winner) {
+			print_winner(winner);
+			return 0;
+		}
+		winning_layout >>= 1;
+	}
+
+	return 0;
 }
